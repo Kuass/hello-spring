587b57cc609464cec81252305c2e738586373c6e
Kua <kua@kakao.com>
2021-03-29 23:08:02 +0900

    jpa 이후 spring jpa 준비

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,27 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
+/*
+    private EntityManager em;
+    private DataSource dataSource;*/
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
+    /*  @Autowired
+        public SpringConfig(DataSource dataSource, EntityManager em) {
+            this.dataSource = dataSource;
+            this.em = em;
+        }
+    */
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
-    @Bean
+ /*   @Bean
     public MemberRepository memberRepository() {
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository();
-    }
+//        return new JpaMemberRepository(em);
+    }*/
 }
