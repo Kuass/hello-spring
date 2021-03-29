40c42ae289a9253bf4bc19dd81b950589028922d
Kua <kua@kakao.com>
2021-03-29 23:20:31 +0900

    Spring Data JPA Test Completed

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -12,31 +12,15 @@ import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
-/*
-    private EntityManager em;
-    private DataSource dataSource;*/
+
     private final MemberRepository memberRepository;
 
-    @Autowired
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-    /*  @Autowired
-        public SpringConfig(DataSource dataSource, EntityManager em) {
-            this.dataSource = dataSource;
-            this.em = em;
-        }
-    */
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
- /*   @Bean
-    public MemberRepository memberRepository() {
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-//        return new JpaMemberRepository(em);
-    }*/
 }
--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -1,12 +1,13 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@Primary
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
-    @Override
     Optional<Member> findByName(String name);
 }
